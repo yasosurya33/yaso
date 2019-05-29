1a2bd1bc6b8699c6c3451b4ca13156149bb08655
Surya S <51106976+yasosurya33@users.noreply.github.com>
2019-05-29 12:31:54 +0530

    Create alphabet.py

---
+++ alphabet.py
@@ -0,0 +1,7 @@
+a=input()
+alphabet = ['a','e','i','o','u','A','E','I','O','U','b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z','B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','Z']
+
+if(a in alphabet) :
+    print('Alphabet')
+else:
+    print('No')
