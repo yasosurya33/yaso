a22f555c664e96f6890d475c9b4f443240e0c42b
Surya S <51106976+yasosurya33@users.noreply.github.com>
2019-06-20 10:36:04 +0530

    Update leap year.py

--- leap year.py
+++ leap year.py
@@ -2,10 +2,10 @@ year= int(input('Enter the year'))
 if (year%4 == 0):
     if (year%100 == 0):
         if(year %400 == 0) :
-            print(f'{year} is a leap year')
+            print('yes')
         else :
-            print(f'{year} is not a leap year')
+            print('no')
     else :
-        print(f'{year} is a leap year')
+        print('yes')
 else :
-    print(f'{year} is not a leap year')
+    print('no')
