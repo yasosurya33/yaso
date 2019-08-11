3a9bf4dc0519cebbe05d66e5c83b84576194ed4e
Surya S <51106976+yasosurya33@users.noreply.github.com>
2019-08-11 12:52:35 +0530

    Update Repeatation of num in list.py

--- Repeatation of num in list.py
+++ Repeatation of num in list.py
@@ -2,8 +2,8 @@ bad=list(map(int,input().split()))[:2]
 good=list(map(int,input().split()))[:4]
 rep=[]
 sum=0
-a=good.count(0)
-b=good.count(1)
+a=good.count(bad[0])
+b=good.count(bad[1])
 if a>b:
     print(a)
 else:
