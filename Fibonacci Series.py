ab80b76b2a3392cc276effd3e422fac987f6c8dd
Surya S <51106976+yasosurya33@users.noreply.github.com>
2019-08-11 10:29:57 +0530

    Create Fibonacci Series.py

---
+++ Fibonacci Series.py
@@ -0,0 +1,14 @@
+num = int(input())
+i = 0
+fir = 0
+sec = 1
+listu = []
+while (i < num):
+    if (i <= 1):
+        Next = i
+    else:
+        Next = fir + sec
+        fir = sec
+        sec = Next
+    print(Next,end=" ")
+    i+=1
