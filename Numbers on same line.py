2a5f727f486c0870046ffa2fa27fa1140757a9d2
Surya S <51106976+yasosurya33@users.noreply.github.com>
2019-08-13 13:42:04 +0530

    Create Numbers on same line.py

---
+++ Numbers on same line.py
@@ -0,0 +1,12 @@
+sam=[]
+for i in range(3):
+    sam.append(list(input().split()))
+lab=0
+for i in range(2):
+    for j in range(1):
+        if not sam[i][j] == sam[i+1][j] and not sam[i][j+1] == sam[i+1][j+1]:
+            lab=1
+if not lab == 1:
+    print("yes")
+else:
+    print("no")
