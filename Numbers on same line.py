7e06f81eaee1e5963c85a10077df68ef5b8d1cd5
Surya S <51106976+yasosurya33@users.noreply.github.com>
2019-08-13 13:44:59 +0530

    Update Numbers on same line.py

--- Numbers on same line.py
+++ Numbers on same line.py
@@ -4,7 +4,7 @@ for i in range(3):
 lab=0
 for i in range(2):
     for j in range(1):
-        if not sam[i][j] == sam[i+1][j] and not sam[i][j+1] == sam[i+1][j+1]:
+        if not sam[i][j] == sam[i+1][j] and not sam[i][j+1] == sam[i+1][j+1] and not sam[i][j] == sam[i][j+1]:
             lab=1
 if not lab == 1:
     print("yes")
