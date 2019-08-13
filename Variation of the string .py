dc486006270df6bf56527c4d5779301ad2d9f2f8
Surya S <51106976+yasosurya33@users.noreply.github.com>
2019-08-13 15:01:48 +0530

    Create Variation of the string .py

---
+++ Variation of the string .py
@@ -0,0 +1,16 @@
+a, b ,c= list(input().split())
+a1=[]
+b2=[]
+for i in a:
+    a1.append(i)
+for i in b:
+    b2.append(i)
+l=len(a1)
+lab=0
+for i in range(l):
+    if not a1[i] in b2:
+        lab+=1
+if lab == int(c[0]):
+    print("yes")
+else:
+    print("no")
